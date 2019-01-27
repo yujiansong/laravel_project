4528cd712cd683a11d7804e6fa868fbdcb5cd521
yjs <yujiansong6@163.com>
2019-01-27 08:14:10 +0000

    modify route

--- routes/web.php
+++ routes/web.php
@@ -375,6 +375,6 @@ Route::get('admin', 'AdminController@index')->name('admin.home');
 Route::get('/login', 'Web\AppController@getLogin')
     ->name('login')
     ->middleware('guest');
-
+//自定义路由
 Route::get('/auth/{social}', 'Web\AuthenticationController@getSocialRedirect')->middleware('guest');
 Route::get('/auth/{social}/callback', 'Web\AuthenticationController@getSocialCallback')->middleware('guest');
